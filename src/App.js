482c7fc5f7269390daa87e3f8d2db2561e613b9e
aknair19 <94102097+aknair19@users.noreply.github.com>
2023-01-31 18:00:57 +0530

    change in id

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
 
   // Add Task
   const addTask = (task) => {
-    const id = task.length;
+    const id = Math.floor(Math.random() * 10000) + 1;
     const newTask = { id, ...task };
     setTasks([...tasks, newTask]);
   };
